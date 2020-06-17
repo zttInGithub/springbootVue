c5fe605f07fd7edb7eacce02e9467c3e093de1e0
ztt <taotao_zhux@163.com>
2020-06-17 09:53:08 +0800

    idea提交

--- src/main/java/com/ztt/test/config/SecurityPermitAllConfig.java
+++ src/main/java/com/ztt/test/config/SecurityPermitAllConfig.java
@@ -2,18 +2,15 @@ package com.ztt.test.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 /**
  * dev 环境加载
  */
-/*@Profile("dev")
-@Configuration
-public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter{
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll()
-			.and().csrf().disable();
-	}
-}*/
+//@Profile("dev")
+//@Configuration
+//public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter{
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests().anyRequest().permitAll()
+//			.and().csrf().disable();
+//	}
+//}
